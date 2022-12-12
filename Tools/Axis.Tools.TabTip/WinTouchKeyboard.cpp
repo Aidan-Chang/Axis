41bc86439010465589f3d42353a2972094e939b9
Aidan Chang <aidan_chang@axos.io>
2022-12-12 14:32:10 +0800

    tabtip

--- Tools/Axis.Tools.TabTip/WinTouchKeyboard.cpp
+++ Tools/Axis.Tools.TabTip/WinTouchKeyboard.cpp
@@ -1,20 +1,9 @@
 #include "WinTouchKeyboard.h"
 
 #include <Objbase.h>
-//#include <atlbase.h>
 #include <initguid.h>
-//#include <tlhelp32.h>
-//#include <windows.h>
 #include <Shobjidl.h>
-
-//#include <algorithm>
-//#include <experimental/filesystem>
 #include <iostream>
-//#include <sstream>
-//#include <string>
-
-//#pragma hdrstop
-//#pragma warning(disable : 4996)
 
 using namespace std;
 
@@ -28,13 +17,6 @@ struct ITipInvocation : IUnknown {
   virtual HRESULT STDMETHODCALLTYPE Toggle(HWND wnd) = 0;
 };
 
-//namespace {
-//  const char* KeyboardWindowClass = "IPTip_Main_Window";
-//  const char* WindowParentClass = "ApplicationFrameWindow";
-//  const char* WindowClass = "Windows.UI.Core.CoreWindow";
-//  const char* WindowCaption = "Microsoft Text Input Application";
-//}
-
 WinTouchKeyboard& WinTouchKeyboard::Instance() {
   static WinTouchKeyboard instance;
   return instance;
@@ -42,56 +24,7 @@ WinTouchKeyboard& WinTouchKeyboard::Instance() {
 
 bool WinTouchKeyboard::OpenScreenKeyboard() { return OpenTabTip(); }
 
-//bool WinTouchKeyboard::OpenOSK() {
-//  PVOID oldValue = NULL;
-//  BOOL bRet = Wow64DisableWow64FsRedirection(&oldValue);
-//  ShellExecuteA(NULL, "open", "osk.exe", NULL, NULL, SW_HIDE);
-//  if (bRet) {
-//    Wow64RevertWow64FsRedirection(oldValue);
-//    return true;
-//  }
-//  else {
-//    return false;
-//  }
-//}
-
 bool WinTouchKeyboard::OpenTabTip() {
-  //if (IsWin10KeyboardVisable() || IsWin7KeyboardVisable()) {
-  //  return true;
-  //}
-  //string tabTipPath = "C:\\Program Files\\Common Files\\Microsoft Shared\\ink\\TabTip.exe";
-  //error_code err;
-  //if (!experimental::filesystem::exists(tabTipPath, err)) {
-  //  return false;
-  //}
-  //if (IsNewVersion()) {
-  //  int pid = 0;
-  //  pid = FindTabTipProcess();
-  //  if (pid == 0) {
-  //    ShellExecuteA(NULL, "open", tabTipPath.c_str(), NULL, NULL, SW_HIDE);
-  //    Sleep(600);
-  //  }
-  //  //if (IsWin10KeyboardVisable() || IsWin7KeyboardVisable()) {
-  //  //  return true;
-  //  //}
-  //  CoInitialize(nullptr);
-  //  CComPtr<ITipInvocation> _tip;
-  //  CoCreateInstance(CLSID_UIHostNoLaunch, 0,
-  //    CLSCTX_INPROC_HANDLER | CLSCTX_LOCAL_SERVER,
-  //    IID_ITipInvocation, (void**)&_tip);
-  //  if (!_tip) {
-  //    CoUninitialize();
-  //    return false;
-  //  }
-  //  else {
-  //    _tip->Toggle(GetDesktopWindow());
-  //  }
-  //}
-  //else {
-  //  ShellExecuteA(NULL, "open", tabTipPath.c_str(), NULL, NULL, SW_HIDE);
-  //}
-  //CoUninitialize();
-  //return true;
   HRESULT hr{ CoInitialize(NULL) };
   if (FAILED(hr)) {
     wcerr << L"Failed to initialize COM." << endl;
@@ -126,111 +59,6 @@ bool WinTouchKeyboard::OpenTabTip() {
   return true;
 }
 
-//bool WinTouchKeyboard::IsWin10KeyboardVisable() {
-//  HWND parent = FindWindowExA(NULL, NULL, WindowParentClass, NULL);
-//  if (!parent) {
-//    return false;
-//  }
-//  HWND wnd = FindWindowExA(parent, NULL, WindowClass, WindowCaption);
-//  if (!wnd) {
-//    return false;
-//  }
-//  return true;
-//}
-//
-//bool WinTouchKeyboard::IsWin7KeyboardVisable() {
-//  HWND touchhWnd = FindWindowA(KeyboardWindowClass, NULL);
-//  if (!touchhWnd) {
-//    return false;
-//  }
-//  unsigned long style = GetWindowLong(touchhWnd, GWL_STYLE);
-//  return (style & WS_CLIPSIBLINGS) && (style & WS_VISIBLE) && (style & WS_POPUP) && !(style & WS_DISABLED);
-//}
-
-//bool WinTouchKeyboard::IsNewVersion() {
-//  string version = GetCurrentVersion();
-//  auto versionList = Split(version, ".");
-//  int compareVersion[] = { 10, 0, 14393 };
-//  for (int i = 0; i < versionList.size(); ++i) {
-//    switch (i) {
-//    case 0:
-//    case 1:
-//    case 2:
-//      if (stoi(versionList[i]) > compareVersion[i]) {
-//        return true;
-//      }
-//      else if (stoi(versionList[i]) < compareVersion[i]) {
-//        return false;
-//      }
-//      break;
-//    case 3:
-//      return true;
-//    }
-//  }
-//  return true;
-//}
-
-//string WinTouchKeyboard::GetCurrentVersion() {
-//  stringstream version;
-//  RTL_OSVERSIONINFOW osv;
-//  osv.dwOSVersionInfoSize = sizeof(osv);
-//  NTSTATUS(WINAPI * pRtlGetVersion)(PRTL_OSVERSIONINFOW) = NULL;
-//  HMODULE hMod = GetModuleHandleW(L"ntdll.dll");
-//  if (hMod != NULL) {
-//    pRtlGetVersion = (NTSTATUS(WINAPI*)(PRTL_OSVERSIONINFOW))GetProcAddress(hMod, "RtlGetVersion");
-//    if (pRtlGetVersion != nullptr) {
-//      pRtlGetVersion(&osv);
-//      version << osv.dwMajorVersion << "." << osv.dwMinorVersion << "." << osv.dwBuildNumber;
-//    }
-//  }
-//  if (pRtlGetVersion == NULL) {
-//    if (GetVersionExW(&osv)) {
-//      version << osv.dwMajorVersion << "." << osv.dwMinorVersion << "." << osv.dwBuildNumber;
-//    }
-//  }
-//  return version.str();
-//}
-
-//vector<string> WinTouchKeyboard::Split(string text, string delimeter) {
-//  vector<string> splittedString;
-//  int startIndex = 0;
-//  int endIndex = 0;
-//  while ((endIndex = text.find(delimeter, startIndex)) < text.size()) {
-//    string val = text.substr(startIndex, endIndex - startIndex);
-//    splittedString.push_back(val);
-//    startIndex = endIndex + delimeter.size();
-//  }
-//  if (startIndex < text.size()) {
-//    string val = text.substr(startIndex);
-//    splittedString.push_back(val);
-//  }
-//  return splittedString;
-//}
-
-//int WinTouchKeyboard::FindTabTipProcess() {
-//  const wchar_t* name = L"TabTip.exe";
-//  HANDLE snapshot;
-//  snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-//  if (INVALID_HANDLE_VALUE == snapshot) {
-//    return 0;
-//  }
-//  PROCESSENTRY32W process;
-//  ZeroMemory(&process, sizeof(process));
-//  process.dwSize = sizeof(process);
-//  BOOL hResult;
-//  hResult = Process32FirstW(snapshot, &process);
-//  int pid = 0;
-//  while (hResult) {
-//    if (_wcsicmp(process.szExeFile, name) == 0) {
-//      pid = process.th32ProcessID;
-//      break;
-//    }
-//    hResult = Process32NextW(snapshot, &process);
-//  }
-//  CloseHandle(snapshot);
-//  return pid;
-//}
-
 bool WinTouchKeyboard::IsInputPaneOpen() {
   RECT rect;
   ZeroMemory(&rect, sizeof(rect));
@@ -243,7 +71,8 @@ bool WinTouchKeyboard::IsInputPaneOpen() {
     }
     frameworkInputPane->Release();
   }
-  if (hr == S_OK)
+  if (hr == S_OK) {
     return true;
+  }
   return false;
 }
--- Tools/Axis.Tools.TabTip/WinTouchKeyboard.h
+++ Tools/Axis.Tools.TabTip/WinTouchKeyboard.h
@@ -1,8 +1,5 @@
 #pragma once
 
-//#include <string>
-//#include <vector>
-
 using namespace std;
 
 class WinTouchKeyboard {
@@ -12,13 +9,6 @@ class WinTouchKeyboard {
   private:
     WinTouchKeyboard() = default;
     ~WinTouchKeyboard() = default;
-    //bool OpenOSK();
     bool OpenTabTip();
-    //bool IsWin10KeyboardVisable();
-    //bool IsWin7KeyboardVisable();
-    //bool IsNewVersion();
-    //vector<string> Split(string text, string delimeter);
-    //string GetCurrentVersion();
-    //int FindTabTipProcess();
     bool IsInputPaneOpen();
 };
