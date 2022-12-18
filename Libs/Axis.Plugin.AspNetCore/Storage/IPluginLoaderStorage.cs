e5abd8575ed5faf7328e579db2d39570ad602b87
Aidan Chang <sinnqek@msn.com>
2022-12-19 07:33:28 +0800

    package update

--- Libs/Axis.Plugin.AspNetCore/Storage/IPluginLoaderStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Axis.Plugin.AspNetCore.Storage;
-
-public interface IPluginLoaderStorage {
-
-  public void Save(Dictionary<string, PluginInfo> collection);
-
-  public Dictionary<string, PluginInfo> Load();
-
-}
