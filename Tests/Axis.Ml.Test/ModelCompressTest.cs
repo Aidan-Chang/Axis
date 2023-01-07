4ce02bbcd3cb77a6644111a9743085d5efb10712
Aidan Chang <aidan_chang@axos.io>
2023-01-07 15:22:09 +0800

    some test senarios

---
+++ Tests/Axis.Ml.Test/ModelCompressTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Axis.Ml.Test;
+
+public class ModelCompressTest {
+
+  [Fact]
+  public void Model_File_Archieve() {
+  }
+
+  [Fact]
+  public void Model_File_Load_From_Archieve() {
+  }
+
+}
---
+++ Tests/Axis.Plugin.Test/PluginLoaderTest.cs
@@ -0,0 +1,16 @@
+namespace Axis.Plugin.Test;
+
+public class PluginLoaderTest {
+
+  [Fact]
+  public void Plugin_Load_And_Reload() {
+
+  }
+
+
+  [Fact]
+  public void Plugin_Load_With_Dependeny_And_Reload() {
+
+  }
+
+}
